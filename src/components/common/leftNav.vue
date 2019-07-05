d38fe6598d88340701344c31b99944f5d1d3622b
vilderlee <1010434086@qq.com>
2019-07-05 18:16:01 +0800

    vilderlee full commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="left-nav">
+         <ul>
+        <li>
+            <i class="icon iconfont iconbuganxingqu"></i>
+            <div>收银</div>
+        </li>
+ 
+        <li>
+            <i class="icon iconfont icondianhua"></i>
+            <div>店铺</div>
+        </li>
+ 
+        <li>
+            <i class="icon iconfont iconaliyu"></i>
+            <div>商品</div>
+        </li>
+ 
+         <li>
+            <i class="icon iconfont iconchenggong"></i>
+            <div>会员</div>
+        </li>
+ 
+ 
+        <li>
+            <i class="icon iconfont iconaixin"></i>
+            <div>统计</div>
+        </li>
+</ul>
+    </div>   
+</template>
+
+<script>
+export default {
+    name: 'leftNav',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+ .left-nav{
+       color:#fff;
+       font-size:10px;
+       height:100%;
+       background-color: #1D8ce0;
+       float:left;
+       width:5%;
+    }
+    .iconfont{
+       font-size:24px;
+    }
+    .left-nav ul{
+        padding:0px;
+        margin: 0px;
+    }
+    .left-nav li{
+        list-style: none;
+        text-align: center;
+        border-bottom:1px solid #20a0ff;
+        padding:10px;
+    }
+</style>
+
